25ac373f5c643211c0fc7c1af2dac818eec0ed5f
pollux.derLux <pollux.derLux@huso.sowieso>
2025-04-04 16:50:58 +0200

    Grauwerthistogramm hinzugefügt

--- analyse.py
+++ analyse.py
@@ -1,23 +1,38 @@
 import argparse
 
 import cv2
+import time
 
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use("TkAgg") 
 
 def main(video):
+    plt.ion()
+    fig, ax = plt.subplots(1,1, figsize=(8,5))
 
     cap = cv2.VideoCapture(video)
     paused = False
 
     while True:
         if not paused:
+            # CV
             ret, img = cap.read()
             if not ret:
                 break # No image available. Exit loop
-
-
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             cv2.imshow('object detection', img) 
             #cv2.resize(im, (640, 640)))
-            
+            hist = cv2.calcHist(img, [0], None, [256], [0, 256])
+
+            # Matplotlib
+            ax.clear()
+            ax.bar(hist)
+
+            plt.draw()
+            plt.pause(0.01)
+
+        # Key Inputs
         kp = cv2.waitKey(1) & 0xFF
         if kp == ord('q'):
             break
